51343e8aad281fe1025b0f28710d09c5c2825935
karasikq <romastepanuik@gmail.com>
2023-11-13 23:13:14 +0300

    Rewrite CLI to new lib features

--- src/bin/cli/libgen_cli.rs
+++ src/bin/cli/libgen_cli.rs
@@ -1,29 +1,30 @@
 use console::Style;
 use dialoguer::{theme::ColorfulTheme, Confirm, FuzzySelect, Input, Select};
-use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
 use lazy_static::lazy_static;
 use reqwest::Client;
-use std::{fs::File, io::Write};
 
-use libgen::api::{
-    book::Book,
-    mirrors::{Mirror, MirrorList, MirrorType},
-    search::{Search, SearchOption},
+use libgen::{
+    api::{
+        book::Book,
+        mirrors::{DownloadMirror, MirrorList, SearchMirror},
+        search::{SearchBuilder, SearchIn},
+    },
+    error::LibgenApiError,
 };
 
 lazy_static! {
     static ref RED_STYLE: Style = Style::new().red();
 }
 
-pub fn select_search_mirror(mirrors: &MirrorList) -> Result<Mirror, &'static str> {
+pub fn select_search_mirror(mirrors: &MirrorList) -> Result<SearchMirror, LibgenApiError> {
     let mirror_selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Search mirror")
         .default(0)
         .items(&mirrors.search_mirrors)
         .interact_opt()
         .unwrap();
-    mirrors.get(MirrorType::Search, mirror_selection.unwrap())
+    mirrors.get_search_mirror(mirror_selection.unwrap())
 }
 
 pub fn input_search_request() -> Result<String, &'static str> {
@@ -46,7 +47,7 @@ const OPTIONS: &[&str; 11] = &[
     "Tags",
     "Extension",
 ];
-pub fn input_search_option() -> Result<SearchOption, &'static str> {
+pub fn input_search_option() -> Result<SearchIn, LibgenApiError> {
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Search option")
         .default(0)
@@ -54,7 +55,7 @@ pub fn input_search_option() -> Result<SearchOption, &'static str> {
         .interact()
         .unwrap();
 
-    SearchOption::try_from(selection)
+    SearchIn::try_from(selection)
 }
 
 pub fn input_results_count() -> Result<u32, &'static str> {
@@ -97,39 +98,42 @@ pub fn print_book_info(book: &Book) -> Result<(), &'static str> {
     Ok(())
 }
 
-pub fn select_download_mirror(mirrors: &MirrorList) -> Result<Mirror, &'static str> {
+pub fn select_download_mirror(mirrors: &MirrorList) -> Result<DownloadMirror, LibgenApiError> {
     let mirror_selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Download mirror")
         .default(0)
         .items(&mirrors.download_mirrors)
         .interact_opt()
         .unwrap();
-    mirrors.get(MirrorType::Download, mirror_selection.unwrap())
+    mirrors.get_download_mirror(mirror_selection.unwrap())
 }
 
-pub async fn init() -> Result<(), &'static str> {
+pub async fn init() -> Result<(), LibgenApiError> {
     let client = Client::new();
-    let mirrors = MirrorList::parse("libgen-rs/mirrors.json");
+    let mirrors = MirrorList::new();
     let Ok(search_mirror) = select_search_mirror(&mirrors) else {
-        return Err("You must select a mirror");
+        return Err("You must select a mirror")?;
     };
     let books = loop {
         let request = input_search_request().expect("Empty request");
         let search_option = input_search_option().unwrap();
         let results = input_results_count().unwrap();
-        let search_options = Search {
-            mirror: search_mirror.clone(),
+        let search_query = SearchBuilder::new(
             request,
-            results,
-            search_option,
-        };
+            search_mirror.search_url.clone(),
+            search_mirror.cover_url.clone(),
+            search_mirror.json_search_url.clone(),
+        )
+        .max_results(results)
+        .search_option(search_option)
+        .build();
         println!("Search at {}... This may take a while", search_mirror);
-        let received_books = search_options.search(&client).await?;
-        if received_books.is_empty() {
+        let search_result = search_query.search().await?;
+        if search_result.is_empty() {
             println!("Books not found");
             continue;
         } else {
-            break received_books;
+            break search_result;
         }
     };
     loop {
@@ -142,36 +146,28 @@ pub async fn init() -> Result<(), &'static str> {
         {
             continue;
         }
-        let download_request = select_download_mirror(&mirrors).unwrap();
-        let down_req = download_request
-            .download_book(&client, &selected_book)
-            .await?;
-        let total_size = down_req.content_length().unwrap();
+        let Ok(download_mirror) = select_download_mirror(&mirrors) else {
+            return Err("You must select a mirror")?;
+        };
 
-        let pb = ProgressBar::new(total_size);
+        let pb = ProgressBar::new(0);
         pb.set_style(ProgressStyle::default_bar()
         .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .unwrap()
         .progress_chars("#>-"));
         pb.set_message("Downloading...");
 
-        let mut book_download_path = dirs::download_dir().unwrap();
-        book_download_path.push("libgen-rs");
-        std::fs::create_dir_all(&book_download_path).unwrap();
-
-        let len = selected_book.title.len().min(249);
-        book_download_path.push(&selected_book.title[0..len]);
-        book_download_path.set_extension(&selected_book.extension);
-        let mut stream = down_req.bytes_stream();
-        let mut file = File::create(book_download_path).unwrap();
-        let mut downloaded: u64 = 0;
-        while let Some(item) = stream.next().await {
-            let chunk = item.or(Err("Error while downloading file")).unwrap();
-            file.write_all(&chunk).unwrap();
-            let new = (downloaded + (chunk.len() as u64)).min(total_size);
-            downloaded = new;
-            pb.set_position(new);
-        }
+        let _ = selected_book
+            .download_to_path(
+                Some(&client),
+                download_mirror,
+                dirs::download_dir().unwrap().to_str().unwrap(),
+                Some(|downloaded, size| {
+                    pb.set_length(size);
+                    pb.set_position(downloaded);
+                }),
+            )
+            .await;
         break;
     }
 
